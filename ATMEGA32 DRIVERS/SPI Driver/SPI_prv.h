2b67513940bb14a5c6fc9a047c722bfc10e1fe88
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-05-31 00:13:25 +0300

    Create SPI_prv.h

---
+++ ATMEGA32 DRIVERS/SPI Driver/SPI_prv.h
@@ -0,0 +1,9 @@
+
+#ifndef INCLUDE_MCAL_SPI_SPI_PRV_H_
+#define INCLUDE_MCAL_SPI_SPI_PRV_H_
+
+#define SPSR *((volatile u8*) 0x2e)
+#define SPCR *((volatile u8*) 0x2d)
+#define SPDR *((volatile u8*) 0x2f)
+
+#endif /* INCLUDE_MCAL_SPI_SPI_PRV_H_ */
